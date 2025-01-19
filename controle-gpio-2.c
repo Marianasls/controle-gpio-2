efbbf58d24619c1d2d70df90c98090f5c6025cad
Clebson Ramos <clebsonramoss@gmail.com>
2025-01-19 20:39:34 -0300

    Adaptação do código para interação com terminal via PuTTY. OBS.: Não consegui conectar-se ao PuTTY e não sei como ele interage com o Raspberry Pi.

--- controle-gpio-2.c
+++ controle-gpio-2.c
@@ -1,18 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 
 #define LED_VERDE 11
 #define LED_AZUL  12
 #define LED_VERMELHO 13
-#define BUZZER    5
+#define BUZZER 5
 
-void desligar_todos_gpio() {
+char captura_de_comando(void){
+    char escolha;
+
+    system("cls");
+    printf("MENU\n\nA - Ligar LED vermelho\nB - Ligar LED verde\nC - Ligar LED azul\nD - Ligar buzzer\n");
+    printf("E - Ligar tudo em sequencia\n\n");
+    escolha = getchar();
+    return escolha;
+}
+
+void desligar_todos_gpio(void){
     gpio_put(LED_VERDE, 0);
     gpio_put(LED_AZUL, 0);
     gpio_put(LED_VERMELHO, 0);
     gpio_put(BUZZER, 0);
 }
 
-void inicializar_dispositivos() {
+void inicializar_dispositivos(void){
     gpio_init(LED_VERDE);
     gpio_init(LED_AZUL);
     gpio_init(LED_VERMELHO);
@@ -26,22 +38,58 @@ void inicializar_dispositivos() {
     desligar_todos_gpio();
 }
 
-int main() {
+int main(void){
+    char escolha;
     stdio_init_all();
     inicializar_dispositivos();
 
     while (true) {
+        escolha = captura_de_comando();
         desligar_todos_gpio();
-        gpio_put(LED_VERDE, 1);
-        sleep_ms(1000);
 
+        switch(escolha){
+            case 'A': // Ligar LED vermelho
+                gpio_put(LED_VERMELHO, 1);
+                printf("LED vermelho acionado!\n");
+                sleep_ms(1000);
+                gpio_put(LED_VERMELHO, 0);
+                break;
+            case 'B': // Ligar LED verde
+                gpio_put(LED_VERDE, 1);
+                printf("LED verde acionado!\n");
+                sleep_ms(1000);
+                gpio_put(LED_VERDE, 0);
+                break;
+            case 'C': // Ligar LED azul
+                gpio_put(LED_AZUL, 1);
+                printf("LED azul acionado!\n");
+                sleep_ms(1000);
+                gpio_put(LED_AZUL, 0);
+                break;
+            case 'D': // Ligar buzzer
+                gpio_put(BUZZER, 1);
+                printf("Buzzer acionado!\n");
+                sleep_ms(1000);
+                gpio_put(BUZZER, 0);
+                break;
+            case 'E': // Ligar tudo em sequência
+                gpio_put(LED_VERMELHO, 1);
+                sleep_ms(600);
+                gpio_put(LED_VERMELHO, 0);
+                gpio_put(LED_VERDE, 1);
+                sleep_ms(600);
+                gpio_put(LED_VERDE, 0);
+                gpio_put(LED_AZUL, 1);
+                sleep_ms(600);
+                gpio_put(LED_AZUL, 0);
+                gpio_put(BUZZER, 1);
+                sleep_ms(600);
+                gpio_put(BUZZER, 0);
+                break;
+            default: // Caso o comando digitado não corresponda a nenhum dos apresentados no menu
+                printf("Comando invalido. Tente novamente.\n");
+        }
         desligar_todos_gpio();
-        gpio_put(LED_AZUL, 1);
-        sleep_ms(1000);
-
-        desligar_todos_gpio();
-        gpio_put(LED_VERMELHO, 1);
-        sleep_ms(1000);
     }
 
     return 0;
