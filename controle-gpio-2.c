b9c95880764f5eb892fac04e174af8a9ef58184f
machadof90 <machadofilho.ti@gmail.com>
2025-01-18 22:41:03 -0300

    adicionadno a funcao de liga todos os leds ao mensmo tempo  eos buzzers, adicionando comentarios ao codigo

--- controle-gpio-2.c
+++ controle-gpio-2.c
@@ -31,21 +31,42 @@ void inicializar_dispositivos() {
 }
 
 int main() {
+    //inicializando os dispositivos
     stdio_init_all();
+
+    //inicializando os pinos
     inicializar_dispositivos();
+    
+    //desligando todos os pinos
     desligar_todos_gpio();
 
-    //
+    //iniciando o loop
     while (true) {
+        //ligando somente o led verde
         desligar_todos_gpio();
         gpio_put(LED_VERDE, 1);
         sleep_ms(1000);
 
+        //ligando somente o led azul
         desligar_todos_gpio();
         gpio_put(LED_AZUL, 1);
         sleep_ms(1000);
 
+        //ligando somente o led vermelho
         desligar_todos_gpio();
+        gpio_put(LED_VERMELHO, 1);
+        sleep_ms(1000);
+
+        //ligando somente o buzzer
+        desligar_todos_gpio();        
+        gpio_put(BUZZER_A, 1);
+        gpio_put(BUZZER_B, 1);
+        sleep_ms(1000);
+
+        //acendendo todos os leds (branco)
+        desligar_todos_gpio();
+        gpio_put(LED_AZUL, 1);
+        gpio_put(LED_VERDE, 1);
         gpio_put(LED_VERMELHO, 1);
         sleep_ms(1000);
     }
