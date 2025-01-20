c5f07f0ea99adf97c00a80280572b9b97920a741
Valter <machadofilho.ti@gmail.com>
2025-01-19 21:05:25 -0300

    Valter (#2)
    * adiciona a pasta build ao .gitignore
    
    * adiciona pino BUZZERs
    
    * adicionadno a funcao de liga todos os leds ao mensmo tempo  eos buzzers, adicionando comentarios ao codigo

--- controle-gpio-2.c
+++ controle-gpio-2.c
@@ -1,45 +1,72 @@
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 #define LED_VERDE 11
 #define LED_AZUL  12
 #define LED_VERMELHO 13
-#define BUZZER    5
+#define BUZZER_A 21 
+#define BUZZER_B 10
 
 void desligar_todos_gpio() {
     gpio_put(LED_VERDE, 0);
     gpio_put(LED_AZUL, 0);
     gpio_put(LED_VERMELHO, 0);
-    gpio_put(BUZZER, 0);
+    gpio_put(BUZZER_A, 0);
+    gpio_put(BUZZER_B, 0);
 }
 
 void inicializar_dispositivos() {
     gpio_init(LED_VERDE);
     gpio_init(LED_AZUL);
     gpio_init(LED_VERMELHO);
-    gpio_init(BUZZER);
+    gpio_init(BUZZER_A);
+    gpio_init(BUZZER_B);
 
     gpio_set_dir(LED_VERDE, GPIO_OUT);
     gpio_set_dir(LED_AZUL, GPIO_OUT);
     gpio_set_dir(LED_VERMELHO, GPIO_OUT);
-    gpio_set_dir(BUZZER, GPIO_OUT);
+    gpio_set_dir(BUZZER_A, GPIO_OUT);
+    gpio_set_dir(BUZZER_B, GPIO_OUT);
 
-    desligar_todos_gpio();
 }
 
 int main() {
+    //inicializando os dispositivos
     stdio_init_all();
-    inicializar_dispositivos();
 
+    //inicializando os pinos
+    inicializar_dispositivos();
+    
+    //desligando todos os pinos
+    desligar_todos_gpio();
+
+    //iniciando o loop
     while (true) {
+        //ligando somente o led verde
         desligar_todos_gpio();
         gpio_put(LED_VERDE, 1);
         sleep_ms(1000);
 
+        //ligando somente o led azul
         desligar_todos_gpio();
         gpio_put(LED_AZUL, 1);
         sleep_ms(1000);
 
+        //ligando somente o led vermelho
         desligar_todos_gpio();
+        gpio_put(LED_VERMELHO, 1);
+        sleep_ms(1000);
+
+        //ligando somente o buzzer
+        desligar_todos_gpio();        
+        gpio_put(BUZZER_A, 1);
+        gpio_put(BUZZER_B, 1);
+        sleep_ms(1000);
+
+        //acendendo todos os leds (branco)
+        desligar_todos_gpio();
+        gpio_put(LED_AZUL, 1);
+        gpio_put(LED_VERDE, 1);
         gpio_put(LED_VERMELHO, 1);
         sleep_ms(1000);
     }
